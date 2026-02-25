ae4c66f6976f8394275f48a367754bdf16cfda3b
Michael Skusa <skusa@infodesign.de>
2026-02-25 13:50:33 +0100

    cleaned up script

--- .github/workflows/build-image.sh
+++ .github/workflows/build-image.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-branch="$( git rev-parse --abbrev-ref HEAD )"
 oci_repository="$( basename `git rev-parse --show-toplevel` )"
 
 docker build \
@@ -11,24 +10,14 @@ docker build \
   --build-arg REVISION="$( git rev-parse --verify HEAD )" \
   --tag "${oci_repository}" .
 
-#trivy image --clear-cache
-#trivy image --exit-code 1 --ignore-policy ./.github/workflows/trivy.rego "${oci_repository}"
-#trivy image --exit-code 0 --ignore-policy ./.github/workflows/trivy.rego "${oci_repository}"
-
 if [[ -z ${GITHUB_ACTIONS} ]]; then
   >&2 echo "Docker push is intended for GitHub actions"
   exit 0
 fi
 
-#if [[ ${branch} != "develop" ]]; then
-#  >&2 echo "Docker push is intended for the develop branch"
-#  exit 0
-#fi
-
 echo "${IMAGE_REGISTRY_TOKEN}" | \
   docker login --username mskusa --password-stdin
 
-#tag="${GITHUB_RUN_NUMBER}"
 tag=latest
 remote_image="mskusa/${oci_repository}:${tag}"
 docker tag "${oci_repository}" "${remote_image}"
